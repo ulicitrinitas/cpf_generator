0161b50193b6809e2b2dfadc7a2a35a4e9eb991a
citrinitas <uli.citrinitas@gmail.com>
2023-09-08 13:25:06 -0300

    criado corpo da função de gerar números aleatórios

--- src/cpf.cpp
+++ src/cpf.cpp
@@ -1,7 +1,31 @@
 #include "cpf.hpp"
 
+#include <cstdint>
+#include <cstdlib>
+#include <sodium/core.h>
+#include <sodium/randombytes.h>
+
 CPF::CPF(std::string nums)
 {
     this->m_numbs = nums;
+}
+
+int rand_gen(int seed)
+{
+    const int buff_size = 64;
+
+    if(sodium_init() == -1){
+        std::cout << "Erro!\n" << "Falha ao gerar números" << '\n';
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[buff_size];
+
+    randombytes_buf(buffer, buff_size);
+
+    uint32_t n = randombytes_uniform(seed);
+
+    return n;
+
 }
 
