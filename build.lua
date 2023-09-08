6c42c0ce52f15f1eaa3cb688f85b46120f4557aa
citrinitas <uli.citrinitas@gmail.com>
2023-09-08 13:41:03 -0300

    adicionada instruções de build

--- build.lua
+++ build.lua
@@ -1,7 +1,7 @@
-local src_dir = "src"
+local src_dir = "src/"
 local libs = "-lpthread -lsodium -lm"
 local cc="clang++"
 local cflags = "-Wall -Werror -Wextra -pedantic -g -fsanitize=address -O2 -o cpf"
 
-
+print("/usr/bin/" .. cc .. libs .. cflags .. src_dir .. "*.cpp")
 
