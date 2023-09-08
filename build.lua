fa376503c58d51f7b569c5bf31fdc739a492a584
citrinitas <uli.citrinitas@gmail.com>
2023-09-08 13:50:32 -0300

    adicionado diretório de build

--- build.lua
+++ build.lua
@@ -1,7 +1,9 @@
 local src_dir = "src/"
 local libs = "-lpthread -lsodium -lm"
 local cc="clang++"
-local cflags = "-Wall -Werror -Wextra -pedantic -g -fsanitize=address -O2 -o cpf"
+local cflags = "-Wall -Werror -Wextra -pedantic -g -fsanitize=address -O2 -o build/cpf"
+
+os.execute("mkdir build")
 
 os.execute(
     "/usr/bin/" .. cc .. " " .. 
