4168354a01be7b3dc8828a2a083ff6d9f7f2a770
citrinitas <uli.citrinitas@gmail.com>
2023-09-08 13:49:05 -0300

    mudado de print pra os.execute

--- build.lua
+++ build.lua
@@ -3,7 +3,7 @@ local libs = "-lpthread -lsodium -lm"
 local cc="clang++"
 local cflags = "-Wall -Werror -Wextra -pedantic -g -fsanitize=address -O2 -o cpf"
 
-print(
+os.execute(
     "/usr/bin/" .. cc .. " " .. 
     libs .. " " .. 
     cflags .. " " .. " " .. 
