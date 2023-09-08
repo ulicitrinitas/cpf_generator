63357b2006ab7fc93b4bda58c233570b845debdb
citrinitas <uli.citrinitas@gmail.com>
2023-09-08 14:11:31 -0300

    renomeado d1 na gen_second_digit

--- src/cpf.cpp
+++ src/cpf.cpp
@@ -16,7 +16,7 @@ CPF::CPF() {
 }
 
 int CPF::gen_first_digit() {
-  int d1;
+  int d1 = 0;
 
   for (size_t i = 0, k = 0; i < this->m_numbs.size(); i++, k--) {
     d1 += std::stoi(this->m_numbs.substr(i, 1)) * k;
@@ -26,13 +26,13 @@ int CPF::gen_first_digit() {
 }
 
 int CPF::gen_second_digit() {
-  int d1;
+  int d2 = 0;
 
   for (size_t i = 0, k = 0; i < this->m_numbs.size() - 1; i++, k--) {
-    d1 += std::stoi(this->m_numbs.substr(i + 1, 1)) * k;
+    d2 += std::stoi(this->m_numbs.substr(i + 1, 1)) * k;
   }
 
-  return (d1 % 11 < 2) ? 0 : 11 - (d1 % 11);
+  return (d2 % 11 < 2) ? 0 : 11 - (d2 % 11);
 }
 
 std::string CPF::get_m_numbs() { return this->m_numbs; }
