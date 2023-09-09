7acdabd908d287bfab4e35139b1c49af87ba575c
citrinitas <uli.citrinitas@gmail.com>
2023-09-09 17:50:40 -0300

    adicionada opção pra checar cpf

--- src/main.cpp
+++ src/main.cpp
@@ -19,10 +19,16 @@ int main(int argc, char **argv) {
       cpf->show();
     }
     else if(args[1] == "--check" || args[1] == "-c"){
-      
+      if(cpf->check(&(args[2]))){
+        std::cout << "\033[32;1m\u2714 CPF válido!\033[m" << '\n';
+      }
+      else{
+        std::cerr << "\033[31;1m\u2716 CPF Inválido ou Parâmetro Inválido" << '\n';
+        return EXIT_FAILURE;
+      }
     }
     else {
-        std::cerr << "Parâmetro Inválido" << '\n';
+        std::cerr << "\033[31;1m\u2716 Parâmetro Inválido" << '\n';
         return EXIT_FAILURE;
     }
 
