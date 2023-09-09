db359e6eddbfb86ca8beebf0426be1dd91a67e2e
citrinitas <uli.citrinitas@gmail.com>
2023-09-09 18:05:57 -0300

    erro ao cehcar cpf corrigido

--- src/main.cpp
+++ src/main.cpp
@@ -11,25 +11,30 @@ int main(int argc, char **argv) {
 
     std::vector<std::string> args;
 
-    for(int i = 0; i < argc; i++){
-      args[i-1] = argv[i];
+    for(int i = 1; i < argc; i++){
+      args.push_back(argv[i]);
     }
 
-    if (args[1] == "--format" || args[1] == "-f") {
+    if (args[0] == "--format" || args[0] == "-f") {
       cpf->show();
     }
-    else if(args[1] == "--check" || args[1] == "-c"){
-      if(cpf->check(&(args[2]))){
-        std::cout << "\033[32;1m\u2714 CPF válido!\033[m" << '\n';
+    else if(args[0] == "--check" || args[0] == "-c"){
+      
+      if(args.size() <= 1){
+        std::cerr << "\033[31;1m\u2716 Parâmetro Inválido \033[m" << '\n';
+        return EXIT_FAILURE;
       }
-      else{
-        std::cerr << "\033[31;1m\u2716 CPF Inválido ou Parâmetro Inválido" << '\n';
+
+      if(cpf->check(&(args[1]))){
+        std::cout << "\033[32;1m\u2714 CPF válido! \033[m" << '\n';
+      } else{
+        std::cerr << "\033[31;1m\u2716 CPF Inválido \033[m" << '\n';
         return EXIT_FAILURE;
       }
     }
     else {
-        std::cerr << "\033[31;1m\u2716 Parâmetro Inválido" << '\n';
-        return EXIT_FAILURE;
+      std::cerr << "\033[31;1m\u2716Parâmetro Inválido\033[m" << '\n';
+      return EXIT_FAILURE;
     }
 
   } else {
