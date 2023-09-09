0caa2cb3ea2d774d0ca3499407a75b3532bc3d68
citrinitas <uli.citrinitas@gmail.com>
2023-09-09 18:06:47 -0300

    adicionada opção na build

--- build.lua
+++ build.lua
@@ -3,7 +3,7 @@ local libs = "-lpthread -lsodium -lm"
 local cc="clang++"
 local cflags = "-Wall -Werror -Wextra -pedantic -g -fsanitize=address -O2 -o build/cpf"
 
-os.execute("mkdir build")
+os.execute("mkdir -p build")
 
 os.execute(
     "/usr/bin/" .. cc .. " " .. 
