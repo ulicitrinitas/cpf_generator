dbdadbbd420bfafbfc2b460622194d47b185de01
citrinitas <uli.citrinitas@gmail.com>
2023-09-09 17:46:08 -0300

    adicionado a usagem de mais de um parâmetro

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,25 @@
 #include "cpf.hpp"
 #include <cstdlib>
 #include <iostream>
+#include <string>
+#include <vector>
 
 int main(int argc, char **argv) {
   auto cpf = std::make_unique<CPF>();
 
   if (argc > 1) {
 
-    std::string args = argv[1];
+    std::vector<std::string> args;
 
-    if (args == "--format" || args == "-f") {
+    for(int i = 0; i < argc; i++){
+      args[i-1] = argv[i];
+    }
+
+    if (args[1] == "--format" || args[1] == "-f") {
       cpf->show();
+    }
+    else if(args[1] == "--check" || args[1] == "-c"){
+      
     }
     else {
         std::cerr << "Parâmetro Inválido" << '\n';
